91b942bb78f49b7d47eac473bd5badc0992f2e29
Tectite <tectitehello@gmail.com>
2023-02-24 22:00:51 -0800

    Using vue js

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+setInterval(function(){
+  count.value += 1
+}, 1000)
+</script>
+
+<template>
+
+  <main>
+tectite hello {{count}}
+  </main>
+</template>
+
+<style scoped>
+</style>
