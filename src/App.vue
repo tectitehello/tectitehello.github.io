adb8f1330f567b45f855a1c9511a394bf2250eb0
Tectite <tectitehello@gmail.com>
2023-02-28 22:16:22 -0800

    added links

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,52 @@ const count = ref(0)
 setInterval(function(){
   count.value += 1
 }, 1000)
+const links = ref([
+  {
+    name: "1",
+    content: "first",
+  },
+  {
+    name: "2",
+    content: "second",
+  },
+  {
+    name: "3",
+    content: "third",
+  },
+])
+function selectlink(link){
+content.value = link.content
+}
+const content = ref("")
+content.value = links.value[0].content
 </script>
 
 <template>
 
   <main>
-tectite hello {{count}}
+    <sidebar>
+      <a v-for="link in links" @click="selectlink(link)" href="#">{{ link.name }}</a>
+    </sidebar>
+    <content>{{ content }}</content>
   </main>
+
 </template>
 
 <style scoped>
+sidebar {
+  display: flex;
+  flex-direction: column;
+  width: 100px;
+}
+sidebar a {
+  margin: 4px 0;
+}
+main {
+  display: flex;
+  width: 600px;
+}
+content {
+  padding: 5px;
+}
 </style>
